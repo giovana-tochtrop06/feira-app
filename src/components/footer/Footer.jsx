f89c83808be36e235c3f77b37bb7f92b5a1b372d
Gi <tochtrop.giovana@gmail.com>
2021-03-09 19:21:50 -0300

    meu primeiro commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = function() {
+    return (
+        <div>
+            blé
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './header.css';
+
+const Header = function() {
+  return(
+      <div className="header">eu sei que tu é a giovana</div>
+  );  
+};
+
+export default Header; 
